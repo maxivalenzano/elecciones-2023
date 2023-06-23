63a9df5f62a8028d6ffee9d57be73b4fe2912f99
Maximiliano Valenzano <MVALENZANO@suppliers.tenaris.com>
2023-06-23 20:44:04 -0300

    se agrega contraseña

--- src/App.js
+++ src/App.js
@@ -1,61 +1,61 @@
 import { AppBar, Button, Toolbar, Typography } from '@mui/material';
 import { Routes, Route, Outlet, Link } from 'react-router-dom';
 import Home from './Home';
-import Mesas from './Mesas';
+// import Mesas from './Mesas';
 import Resultados from './Resultados';
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="resultados" element={<Resultados />} />
-        <Route path="mesas" element={<Mesas />} />
+    return (
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path='resultados' element={<Resultados />} />
+                <Route path='mesas' element={<Home />} />
 
-        <Route path="*" element={<NoMatch />} />
-      </Route>
-    </Routes>
-  );
+                <Route path='*' element={<NoMatch />} />
+            </Route>
+        </Routes>
+    );
 }
 
 function Layout() {
-  return (
-    <div>
-      <AppBar position="static" color="primary">
-        <Toolbar>
-          <Typography
-            variant="h6"
-            component={Link}
-            to="/"
-            sx={{ flexGrow: 1, color: 'white', textDecoration: 'none' }}
-          >
-            Elecciones 2023
-          </Typography>
-          <div>
-            <Button component={Link} to="/resultados" color="inherit">
-              Resultados
-            </Button>
-            <Button component={Link} to="/mesas" color="inherit">
-              Mesas
-            </Button>
-          </div>
-        </Toolbar>
-      </AppBar>
+    return (
+        <div>
+            <AppBar position='static' color='primary'>
+                <Toolbar>
+                    <Typography
+                        variant='h6'
+                        component={Link}
+                        to='/'
+                        sx={{ flexGrow: 1, color: 'white', textDecoration: 'none' }}
+                    >
+                        Elecciones 2023
+                    </Typography>
+                    <div>
+                        <Button component={Link} to='/resultados' color='inherit'>
+                            Resultados
+                        </Button>
+                        {/* <Button component={Link} to='/mesas' color='inherit'>
+                            Mesas
+                        </Button> */}
+                    </div>
+                </Toolbar>
+            </AppBar>
 
-      <hr />
+            <hr />
 
-      <Outlet />
-    </div>
-  );
+            <Outlet />
+        </div>
+    );
 }
 
 function NoMatch() {
-  return (
-    <div>
-      <h2>Nothing to see here!</h2>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
-  );
+    return (
+        <div>
+            <h2>Nothing to see here!</h2>
+            <p>
+                <Link to='/'>Go to the home page</Link>
+            </p>
+        </div>
+    );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,17 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { Box, Button, Container, InputLabel, Select, TextField, Typography } from '@mui/material';
+import {
+    Box,
+    Button,
+    Container,
+    InputLabel,
+    Select,
+    TextField,
+    Typography,
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+} from '@mui/material';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, child, get, update } from 'firebase/database';
 import { groupBy } from 'lodash';
@@ -8,157 +20,211 @@ import firebaseConfig from './firebaseConfig';
 const MAX_VOTOS = 350;
 
 const Home = () => {
-  const [mesas, setMesas] = useState([]);
-  const [groupedByLugar, setGroupedByLugar] = useState([]);
-  const [selectedCandidatos, setSelectedCandidatos] = useState([]);
-  const [selectedMesa, setSelectedMesa] = useState('');
-  const [loading, setLoading] = useState(false);
-  const [saved, setSaved] = useState(false);
-  const app = initializeApp(firebaseConfig);
-  const database = getDatabase(app);
+    const app = initializeApp(firebaseConfig);
+    const database = getDatabase(app);
+    const [mesas, setMesas] = useState([]);
+    const [groupedByLugar, setGroupedByLugar] = useState([]);
+    const [selectedCandidatos, setSelectedCandidatos] = useState([]);
+    const [selectedMesa, setSelectedMesa] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [saved, setSaved] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [password, setPassword] = useState('');
+    const [isEdit, setIsEdit] = useState(false);
+    const [error, setError] = useState(false);
 
-  const fetchData = useCallback(() => {
-    const dbRef = ref(database);
-    get(child(dbRef, 'mesas/-NY_H_gWJmIzqDE9pGG9/listado'))
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          const response = snapshot.val();
-          const auxGroupedData = groupBy(response, 'lugar');
-          setGroupedByLugar(auxGroupedData);
-          setMesas(response);
-          console.log('Home ~ fetchData ~ GroupedData:', auxGroupedData);
+    const handleEdit = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+        setError(false);
+        setPassword('');
+    };
+
+    const handlePasswordChange = (event) => {
+        setError(false);
+        setPassword(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        if (password === '7palmas') {
+            setIsEdit(true);
+            setOpen(false);
         } else {
-          console.log('No data available');
+            setPassword('');
+            setError(true);
         }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [database]);
+    };
 
-  useEffect(() => {
-    fetchData();
-  }, [fetchData]);
+    const fetchData = useCallback(() => {
+        const dbRef = ref(database);
+        get(child(dbRef, 'mesas/-NY_H_gWJmIzqDE9pGG9/listado'))
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    const response = snapshot.val();
+                    const auxGroupedData = groupBy(response, 'lugar');
+                    setGroupedByLugar(auxGroupedData);
+                    setMesas(response);
+                    console.log('Home ~ fetchData ~ GroupedData:', auxGroupedData);
+                } else {
+                    console.log('No data available');
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [database]);
 
-  const handleChange = (event) => {
-    setSaved(false);
-    setLoading(true);
-    const findCandidatos = mesas.find((item) => item.numero.toString() === event.target.value);
-
-    setSelectedMesa(event.target.value);
-    setSelectedCandidatos(findCandidatos?.candidatos || []);
-  };
-
-  const handleVotosChange = ({ target: { value } }, candidatoId) => {
-    setSaved(false);
-    setLoading(false);
-    const updatedCandidatos = selectedCandidatos.map((candidato) => {
-      if (candidato.id === candidatoId) {
-        let inputVotos = parseInt(value);
-
-        if (inputVotos < 0) {
-          inputVotos = 0;
-        } else if (inputVotos > MAX_VOTOS) {
-          inputVotos = MAX_VOTOS;
-        }
-
-        return { ...candidato, votos: inputVotos };
-      }
-      return candidato;
-    });
-    setSelectedCandidatos(updatedCandidatos);
-  };
-
-  const handleSaveVotosChange = () => {
-    setLoading(true);
-    const updatedCandidatos = selectedCandidatos.reduce((acc, candidato) => {
-      acc[`${candidato.id}/votos`] = candidato.votos || 0;
-      return acc;
-    }, {});
-    const mesaId = mesas.find((item) => item.numero.toString() === selectedMesa).id;
-
-    const dbRef = ref(database);
-
-    update(
-      child(dbRef, `mesas/-NY_H_gWJmIzqDE9pGG9/listado/${mesaId}/candidatos`),
-      updatedCandidatos
-    )
-      .then(() => {
-        console.log('Candidatos actualizados correctamente.');
+    useEffect(() => {
         fetchData();
-      })
-      .catch((error) => {
-        console.error('Error al actualizar los candidatos:', error);
-      })
-      .finally(() => {
-        setLoading(false);
-        setSaved(true);
-      });
-  };
+    }, [fetchData]);
 
-  return (
-    <Container>
-      <Box pb={2} alignItems="center">
-        <InputLabel shrink={true} id="select-label">
-          Seleccionar mesa
-        </InputLabel>
-        <Select
-          native
-          labelId="select-label"
-          fullWidth
-          value={selectedMesa}
-          onChange={handleChange}
-        >
-          <option disabled value="">
-            Seleccione una mesa
-          </option>
-          {Object.entries(groupedByLugar).map(([lugar, mesas]) => {
-            return (
-              <optgroup label={lugar} key={lugar}>
-                {mesas.map((mesa) => (
-                  <option key={mesa.numero} value={mesa.numero}>
-                    {mesa.nombre}
-                  </option>
-                ))}
-              </optgroup>
-            );
-          })}
-        </Select>
-      </Box>
-      <Box>
-        {selectedMesa &&
-          selectedCandidatos.map((candidato) => {
-            return (
-              <Box key={candidato.id} display="flex" alignItems="center" py={2}>
-                <Box minWidth={80}>
-                  <Typography>{candidato.nombre}</Typography>
-                </Box>
-                <TextField
-                  type="number"
-                  id={`votos-${candidato.id}`}
-                  label="Votos"
-                  value={candidato.votos}
-                  onChange={(event) => handleVotosChange(event, candidato.id)}
-                />
-              </Box>
-            );
-          })}
-      </Box>
-      <Box alignItems="center" py={2}>
-        {selectedMesa && (
-          <Button
-            disabled={loading || saved}
-            variant="contained"
-            fullWidth
-            onClick={handleSaveVotosChange}
-            style={{ backgroundColor: saved ? 'green' : null, color: saved ? 'white' : null }}
-          >
-            {saved ? 'Guardado' : 'Guardar'}
-          </Button>
-        )}
-      </Box>
-    </Container>
-  );
+    const handleChange = (event) => {
+        setSaved(false);
+        setLoading(true);
+        setIsEdit(false);
+        const findCandidatos = mesas.find((item) => item.numero.toString() === event.target.value);
+
+        setSelectedMesa(event.target.value);
+        setSelectedCandidatos(findCandidatos?.candidatos || []);
+    };
+
+    const handleVotosChange = ({ target: { value } }, candidatoId) => {
+        setSaved(false);
+        setLoading(false);
+        const updatedCandidatos = selectedCandidatos.map((candidato) => {
+            if (candidato.id === candidatoId) {
+                let inputVotos = parseInt(value);
+
+                if (inputVotos < 0) {
+                    inputVotos = 0;
+                } else if (inputVotos > MAX_VOTOS) {
+                    inputVotos = MAX_VOTOS;
+                }
+
+                return { ...candidato, votos: inputVotos };
+            }
+            return candidato;
+        });
+        setSelectedCandidatos(updatedCandidatos);
+    };
+
+    const handleSaveVotosChange = () => {
+        setLoading(true);
+        const updatedCandidatos = selectedCandidatos.reduce((acc, candidato) => {
+            acc[`${candidato.id}/votos`] = candidato.votos || 0;
+            return acc;
+        }, {});
+        const mesaId = mesas.find((item) => item.numero.toString() === selectedMesa).id;
+
+        const dbRef = ref(database);
+
+        update(child(dbRef, `mesas/-NY_H_gWJmIzqDE9pGG9/listado/${mesaId}/candidatos`), updatedCandidatos)
+            .then(() => {
+                console.log('Candidatos actualizados correctamente.');
+                fetchData();
+            })
+            .catch((error) => {
+                console.error('Error al actualizar los candidatos:', error);
+            })
+            .finally(() => {
+                setLoading(false);
+                setSaved(true);
+            });
+    };
+
+    return (
+        <Container>
+            <Box pb={2} alignItems='center'>
+                <InputLabel shrink={true} id='select-label'>
+                    Seleccionar mesa
+                </InputLabel>
+                <Select native labelId='select-label' fullWidth value={selectedMesa} onChange={handleChange}>
+                    <option disabled value=''>
+                        Seleccione una mesa
+                    </option>
+                    {Object.entries(groupedByLugar).map(([lugar, mesas]) => {
+                        return (
+                            <optgroup label={lugar} key={lugar}>
+                                {mesas.map((mesa) => (
+                                    <option key={mesa.numero} value={mesa.numero}>
+                                        {mesa.nombre}
+                                    </option>
+                                ))}
+                            </optgroup>
+                        );
+                    })}
+                </Select>
+            </Box>
+            <Box>
+                {selectedMesa &&
+                    selectedCandidatos.map((candidato) => {
+                        return (
+                            <Box key={candidato.id} display='flex' alignItems='center' py={2}>
+                                <Box minWidth={80}>
+                                    <Typography>{candidato.nombre}</Typography>
+                                </Box>
+                                <TextField
+                                    variant={isEdit ? 'outlined' : 'standard'}
+                                    type='number'
+                                    id={`votos-${candidato.id}`}
+                                    label='Votos'
+                                    value={candidato.votos}
+                                    InputProps={{ readOnly: !isEdit }}
+                                    onChange={(event) => handleVotosChange(event, candidato.id)}
+                                />
+                            </Box>
+                        );
+                    })}
+            </Box>
+            <Box alignItems='center' py={2}>
+                {selectedMesa && (
+                    <Box>
+                        <Button disabled={isEdit} variant='contained' fullWidth onClick={handleEdit}>
+                            Editar
+                        </Button>
+                        <Button
+                            disabled={loading || saved}
+                            variant='contained'
+                            fullWidth
+                            onClick={handleSaveVotosChange}
+                            style={{
+                                backgroundColor: saved ? 'green' : null,
+                                color: saved ? 'white' : null,
+                                marginTop: '8px',
+                            }}
+                        >
+                            {saved ? 'Guardado' : 'Guardar'}
+                        </Button>
+                    </Box>
+                )}
+            </Box>
+            <Dialog open={open} onClose={handleClose} fullWidth>
+                <DialogTitle>Ingrese la contraseña</DialogTitle>
+                <DialogContent>
+                    <Box py={1}>
+                        <TextField
+                            type='password'
+                            label='Contraseña'
+                            value={password}
+                            fullWidth
+                            onChange={handlePasswordChange}
+                            error={error}
+                            helperText={error && 'Contraseña incorrecta'}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancelar</Button>
+                    <Button variant='contained' onClick={handleSubmit} autoFocus>
+                        Confirmar
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </Container>
+    );
 };
 
 export default Home;
